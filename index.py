c78406bd608a9fa51ef7caf19c0381b4601bf326
politrax <sami.benyounes.pro@gmail.com>
2020-10-02 18:27:12 +0200

    azf

--- index.py
+++ index.py
@@ -1,15 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('http://fanfox.net/manga')
-soup = BeautifulSoup(page.text, 'html.parser')
-
 urls_manga = []
 
 
-for p in soup.findAll('p', 'manga-list-1-item-title'):
-	for a in p.select('a[href]'):
-		urls_manga.append(a['href'].split('/manga')[1])
+def get_manga_of_page():
+	page = requests.get('http://fanfox.net/manga')
+	soup = BeautifulSoup(page.text, 'html.parser')
+	max_page = BeautifulSoup(page.text, 'html.parser').find()
+	for p in soup.findAll('p', 'manga-list-1-item-title'):
+		for a in p.select('a[href]'):
+			urls_manga.append(a['href'].split('/manga')[1])
 
-print(urls_manga)
 
+# get_manga_of_page()
+# print(urls_manga)
+
+page = requests.get('http://fanfox.net/manga')
+soup = BeautifulSoup(page.text, 'html.parser')
+x = soup.select(".pager-list-left")
+y = x[x.length - 1]
+print(y)
